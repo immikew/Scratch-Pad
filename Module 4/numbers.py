27aa6502f4c7cfbc52abecfda62d2e4e5c161c32
Mike Woods <im.mike.woods@gmail.com>
2020-09-26 16:40:29 -0700

    Module 4

--- Module 4/numbers.py
+++ Module 4/numbers.py
@@ -1,11 +1,54 @@
 
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
 
 
-    age = 32
-
-    print(age)
-    print(type(age))
+    # age = 32
+    # print(age)
+    # print(type(age))
     
+    # str_age = '32'
+    # age = int(str_age)
+    # print(age)
+    # print(type(age))
+
+    # age_bin_str = '100000'
+    # age = int(age_bin_str, 2)
+    # print(age)
+    # print(type(age))
+
+    # age_hex_str = '20'
+    # age = int(age_hex_str, 16)
+    # print(age)
+    # print(type(age))
+
+    # one_billion = 1_000_000_000
+    # print(one_billion)
+
+
+
+    # age = 32.3
+    # age = '32.3'
+    # age = float(age)
+
+    # one_fillion = 1_000_000_000.0
+
+    # really_small_number = 3.14e-12
+    # print(really_small_number)
+
+
+    # too_big = -2e400
+    # print(too_big)
+
+    
+    # float_val = .1 + .1 + .1
+    # print(float_val)
+
+
+    my_complex_num = 2 + 3j
+    print(my_complex_num.real)
+    print(my_complex_num.imag)
+    print(my_complex_num.conjugate())
+
+    print('script complete')
---
+++ Module 4/numerical_operators.py
@@ -0,0 +1,37 @@
+
+
+
+if __name__ == "__main__":
+
+    # Addition
+    result = 3 + 9
+    result = -3 + 4.0
+
+    # Subtraction
+    result = -3 - 4.0
+    result = -10.0 - 4
+
+    # Multiplication
+    result = 4 * 3
+    result = 6 * -2.1
+
+    # Division
+    result = 4 / 3
+    result = 8 / 3.0
+
+    # Integer Division
+    result = 5 // 3
+
+    # Modulus (Remainder)
+    remainder = 5 % 3
+
+    # result = 5 / 0
+
+    # Exponentiation
+    result = 2 ** 3
+    result = 9 ** 0.5
+    result = 27 ** (1 / 3)
+
+
+
+    print('script complete')
