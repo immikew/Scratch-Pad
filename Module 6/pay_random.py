d1d16d5b9413bf2fc324177a9ed1ef2bb2fc86f2
Mike Woods <im.mike.woods@gmail.com>
2020-10-18 15:50:57 -0700

    Update pay_random.py
    Finished Mod 6

--- Module 6/pay_random.py
+++ Module 6/pay_random.py
@@ -1,13 +1,22 @@
 
-GROSS_PAY_DEFAULT = 500.0
-grosspay = 500.0
 
-def caluclate_weeks_pay(hours, pay):
-    grosspay = hours * pay
-    print(f'grosspay AFTER assignment in function: {grosspay}')
-    netpay = grosspay * .90
-    return netpay, grosspay
+# what if we wanted to have the ability to change tax rate?
+# but still have the defualt be 0.10
+def calculate_weeks_pay(hours=30, pay=15.25, tax_rate=0.15, **kwargs):
+    print(kwargs)
+    gross = hours * pay
+    net_rate = 1 - tax_rate
+    net = gross * net_rate
+    return net, gross
 
-print(caluclate_weeks_pay(30, 15.25))
+# print(calculate_weeks_pay(30, tax_rate=0.15))
 
-print(f'gross pay AT END: {grosspay}')
+weeks_summary = {
+    'hours': 25,
+    'pay': 16.00,
+    'tax_rate': 0.10,
+    'week_number': 26,
+    'year': 2019
+}
+
+print(calculate_weeks_pay(**weeks_summary))
